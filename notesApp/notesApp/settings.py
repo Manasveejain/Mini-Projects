c54eb6eb388e8398db83882e2b62072e60797f64
Manasvee jain <mansveejain2005@gmail.com>
2025-12-11 20:09:30 +0530

    Update settings.py and add requirements.txt

--- notesApp/notesApp/settings.py
+++ notesApp/notesApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4r)#c&5&b9b(hmngl_9bxcgts9z_qod5buy$g*cl^7jxyi%mn_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +128,4 @@ LOGOUT_REDIRECT_URL = 'login'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
